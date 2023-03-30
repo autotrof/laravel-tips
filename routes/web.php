c96cb7c39da3fd8a2487f063a001ce183e48c03f
autotrof <upload.kurniawan@gmail.com>
2023-03-31 06:29:35 +0700

    dark mode simple

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/tes', [GeneralController::class, 'index']);
+Route::get('/', [GeneralController::class, 'index']);
